7b1e70c0668e61116ce3f757f18afd35dfec4306
Mike <[omitted]>
2020-03-14 00:15:52 -0500

    added AlphabetSoup

---
+++ AlphabetSoup/Main.java
@@ -0,0 +1,15 @@
+package com.packages;
+
+public class Main {
+
+    //main method
+    public static void main(String[] args) {
+
+        //create new game board object
+        Board gameBoard = new Board();
+
+        //find word in game board and print location
+        gameBoard.findWords();
+
+    }
+}
