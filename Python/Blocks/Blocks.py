5d22bba5628c9a05a193b6eb8a5a2aa0f427827f
Mike <[omitted]>
2022-10-17 16:46:01 -0700

    Added files for new app 'Blocks.py'

---
+++ Python/Blocks/Blocks.py
@@ -0,0 +1,79 @@
+#!/bin/python3
+
+import os
+import sys
+import requests
+from time import sleep, time
+from datetime import timedelta
+from playsound import playsound
+
+api_url = 'https://mempool.space/api/'
+block = 'block/'
+hash = 'blocks/tip/hash'
+height = 'blocks/tip/height'
+
+sound = 'sound.wav'
+
+red = '\033[31m'
+wht = '\033[0m'
+
+# Returns int: Current highest block number
+
+
+def getHeight():
+    call = api_url + height
+    return requests.get(call).json()
+
+
+# Returns int: Seconds passed since last block mined
+def getTimeSinceBlock():
+    call = api_url + hash
+    thisHash = requests.get(call).text
+    call = api_url + block + thisHash
+    seconds = requests.get(call).json()
+
+    return int(time()) - seconds['timestamp']
+
+
+# Accepts int Seconds, formats and prints seconds elapsed
+def printTime(seconds):
+    time = str(timedelta(seconds=seconds))
+    output = ''
+    if seconds < 600:
+        output = time
+    else:
+        output = red + time + wht
+
+    print('\r\033[K' + output, end='')
+
+
+if __name__ == '__main__':
+    os.system('clear')
+
+    thisBlock = getHeight()
+    print('CURRENT BLOCK:   ' + str(thisBlock))
+    playsound('/Users/mike/Desktop/' + sound)
+
+    seconds = getTimeSinceBlock()
+
+    while True:
+        printTime(seconds)
+
+        # Check for new block every 15 seconds
+        if seconds % 15 == 0:
+            response = getHeight()
+
+            # If new block is mined
+            if response > thisBlock:
+                # Calculate and print elapsed block time
+                elapsed = seconds
+                seconds = getTimeSinceBlock()
+                printTime(elapsed-seconds)
+
+                # Update and print new block height
+                thisBlock = response
+                print('\nNEW BLOCK MINED: ' + str(thisBlock))
+                playsound('/Users/mike/Desktop/' + sound)
+
+        sleep(1)
+        seconds += 1
