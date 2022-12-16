6005e452f9e4329399167458c1fddd3c8319f0c0
Mike <mikehess1@gmail.com>
2022-12-16 12:39:02 -0500

    corrected duplicate results bug in Rentals.py

--- Python/Rentals/Rentals.py
+++ Python/Rentals/Rentals.py
@@ -18,7 +18,7 @@ def getHour(hour):
 
 # format and return api_url
 def getAPI_URL(hour):
-    api_url = "http://api.hotwire.com/v1/search/car?apikey=[key]&format=json&dest=MIA&dropOffDest=MCO&startdate=11/20/2022&enddate=11/21/2022&pickuptime=[hour]:00&dropofftime=[hour]:00"
+    api_url = "http://api.hotwire.com/v1/search/car?apikey=[key]&format=json&dest=SAN&dropOffDest=SAN&startdate=12/19/2022&enddate=12/23/2022&pickuptime=[hour]:00&dropofftime=[hour]:00"
     return api_url.replace('[key]', api_key).replace('[hour]', str(hour))
 
 
@@ -38,33 +38,35 @@ if __name__ == '__main__':
     # declare price list, first and last hour (in 24 hour format)
     prices = []
     first_hour = 8
-    last_hour = 18
+    last_hour = 12
     page = 1
 
     # for each hour, first to last, request API data and parse
     for hour in range(first_hour, last_hour + 1):
 
+        # print page status
         print('Processing Page', page, end='')
         page += 1
 
-        # make API get request and store response
+        # make get request to API and store response
         response = getJSON(hour)
 
         # for each result in response, check for cheaper result and update if found
-        for item in response['Result']:
-            # store price of current result
-            price = float(item['TotalPrice'])
+        if 'Result' in response:
+            for item in response['Result']:
+                # store price of current result
+                price = float(item['TotalPrice'])
 
-            # add first 3 results to prices list and sort
-            if len(prices) < 3:
-                prices.append((price, item))
-                prices.sort(key=lambda p: p[0])
-            else:
-                # if cheaper option is found, add and remove most expensive
-                for n in range(len(prices[0])):
-                    if price < prices[n][0]:
-                        prices.insert(n, (price, item))
-                        prices.pop()
+                # add first 3 results to prices list and sort
+                if len(prices) < 3:
+                    prices.append((price, item))
+                    prices.sort(key=lambda p: p[0])
+                else:
+                    # if cheaper option is found, add and remove most expensive
+                    for n in range(len(prices[0])):
+                        if price < prices[n][0] and price != prices[n-1][0]:
+                            prices.insert(n, (price, item))
+                            prices.pop()
 
         # limit maximum of 5 requests per second
         sleep(.2)
@@ -72,7 +74,9 @@ if __name__ == '__main__':
 
     # print results
     os.system('clear')
+
     for p in prices:
+        print("----------------------")
         print('Price: $'+'%.2f' % p[0])
         print('Pickup:', p[1]['PickupTime'])
         print('Direct Link:\n' + p[1]['DeepLink'] + '\n')
